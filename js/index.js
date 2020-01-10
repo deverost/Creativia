ff2703527f63153edf0ec8e69c51fc0cbaeb01d1
Rostislav <deverost@gmail.com>
2020-01-10 03:10:41 +0100

    carousel is fixed

--- js/index.js
+++ js/index.js
@@ -2,25 +2,25 @@
 let pluses = document.getElementsByClassName('circle-plus');
 let tabs = document.getElementsByClassName('accordion__tab');
 
-for (let i = 0; i < pluses.length; i++) {
-  pluses[i].addEventListener('click', function () {
-    this.classList.toggle('opened')
-  })
-}
+// for (let i = 0; i < pluses.length; i++) {
+//   pluses[i].addEventListener('click', function () {
+//     this.classList.toggle('opened')
+//   })
+// }
 
 for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener('click', function () {
-    if (i===0 || i === 1) {
+    if (i===0) {
       pluses[0].classList.toggle('opened');
       pluses[1].classList.remove('opened');
       pluses[2].classList.remove('opened');
     }
-    else if (i===2 || i === 3) {
+    else if (i===1) {
       pluses[1].classList.toggle('opened');
       pluses[0].classList.remove('opened');
       pluses[2].classList.remove('opened');
     }
-    else if (i===4 || i === 5) {
+    else if (i===2) {
       pluses[2].classList.toggle('opened');
       pluses[0].classList.remove('opened');
       pluses[1].classList.remove('opened');
@@ -28,24 +28,45 @@ for (let i = 0; i < tabs.length; i++) {
   });
 }
 
+
+
+
+let tabsClass = document.getElementsByClassName('q');
+let contents = document.getElementsByClassName("accordion-content");
+let tabArray = [];
+
+for (let i = 0; i < tabsClass.length; i++) {
+  tabArray.push(tabsClass[i]);
+
+  tabArray[i].addEventListener('click', function (e) {
+    e.preventDefault();
+    // contents[i].classList.toggle('accordion-content--open');
+    tabArray[i].classList.toggle('accordion-content--open');
+    console.log(contents[i])
+    let closeTabs = tabArray.filter(tab => tab !== tabArray[i]);
+
+    for (let tab of closeTabs){
+      tab.classList.remove('accordion-content--open');    }
+  })
+}
 let logos = document.getElementsByClassName("footer__socialLogo");
-let logosFiltered = [];
+let logosArray = [];
 
 for (let i = 0; i < logos.length; i++) {
-  logosFiltered.push(logos.item(i));
+  logosArray.push(logos.item(i));
 
   logos[i].addEventListener("mouseover", function() {
 
-    let changeLogos = logosFiltered.filter(logo => logo != logos[i]);
+    let changeLogos = logosArray.filter(logo => logo !== logos[i]);
 
-    for (logo of changeLogos) {
+    for (let logo of changeLogos) {
       logo.classList.add("footer__socialLogo_hover")
     }
 
   });
 
   logos[i].addEventListener("mouseout", function() {
-    for (logo of logosFiltered) {
+    for (let logo of logosArray) {
       logo.classList.remove("footer__socialLogo_hover");
     }
   });
